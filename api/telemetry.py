f8ce2053c85865d19d2ed89e52e58f1b5948fc74
Seth Juarez <me@sethjuarez.com>
2025-05-30 14:54:47 -0700

    upgrade logging to DEBUG

--- api/telemetry.py
+++ api/telemetry.py
@@ -74,7 +74,7 @@ def init_tracing(local_tracing: bool = True):
         Tracer.add("OpenTelemetry", otel_genai_mapper.trace_span)
 
         azmon_logger = logging.getLogger("azure")
-        azmon_logger.setLevel(logging.INFO)
+        azmon_logger.setLevel(logging.DEBUG)
 
         # oteltrace.set_tracer_provider(TracerProvider())
 
